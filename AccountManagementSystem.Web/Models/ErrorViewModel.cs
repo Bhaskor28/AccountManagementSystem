9c95b209739ac2cf84c07e9a0ab8cd32dafeb910
Bhaskor <bhaskorcse520@gmail.com>
2025-06-30 00:42:40 +0600

    Add project files.

---
+++ AccountManagementSystem.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AccountManagementSystem.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
